ade29065e45ec85e75a370cadc2423ccbba725a0
wjfarfan <wjfarfan@sudamericano.edu.ec>
2026-01-28 20:35:22 -0500

    se creo la pestaña en vivo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonIcon, IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
   // 1. Declaramos la variable que usas en el HTML
